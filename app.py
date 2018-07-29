caed0180c13fbf34c0ce49b23b0f41f8c65335bb
Max Zheng <34936931+54bp6cl6@users.noreply.github.com>
2018-07-29 20:30:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,16 +33,12 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TemplateSendMessage(alt_text='此訊息為特殊訊息，請到手機版查看',template=ConfirmTemplate(text='你好嗎',
+    message = TemplateSendMessage(alt_text='此訊息為特殊訊息，請到手機版查看',template=ConfirmTemplate(text=message.type,
         actions=[PostbackTemplateAction(label='很好',data='hi'),
             PostbackTemplateAction(label='不好',data='bad')]))
     line_bot_api.reply_message(event.reply_token, message)
 
-@handler.add(PostbackEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "喔是喔"))
-    
-    
+
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
