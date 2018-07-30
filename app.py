2836d2c0a60551a6ac6bf231b44fd81be1365c63
Max Zheng <34936931+54bp6cl6@users.noreply.github.com>
2018-07-30 08:11:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -34,28 +34,28 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TemplateSendMessage(
-    alt_text='Buttons template',
-    template=ButtonsTemplate(
-        thumbnail_image_url='https://cc.tvbs.com.tw/img/program/upload/2018/03/05/20180305124010-4c9c5715.jpg',
-        title='Menu',
-        text='我帥嗎',
-        actions=[
-            PostbackTemplateAction(
-                label='蠻帥的',
-                data='good'
-            ),
-            MessageTemplateAction(
-                label='還好',
-                text='我覺得普通'
-            ),
-            URITemplateAction(
-                label='不行',
-                uri='http://cdn.clickme.net/Gallery/2017/08/03/2c04861ba0c20c428572205919b0f127.jpg'
-            )
-        ]
+        alt_text='Buttons template',
+        template=ButtonsTemplate(
+            thumbnail_image_url='https://cc.tvbs.com.tw/img/program/upload/2018/03/05/20180305124010-4c9c5715.jpg',
+            title='Menu',
+            text='我帥嗎',
+            actions=[
+                PostbackTemplateAction(
+                    label='蠻帥的',
+                    data='good'
+                ),
+                MessageTemplateAction(
+                    label='還好',
+                    text='我覺得普通'
+                ),
+                URITemplateAction(
+                    label='不行',
+                    uri='http://cdn.clickme.net/Gallery/2017/08/03/2c04861ba0c20c428572205919b0f127.jpg'
+                )
+            ]
+        )
     )
-)
-line_bot_api.reply_message(event.reply_token, message)
+    line_bot_api.reply_message(event.reply_token, message)
 
 @handler.add(PostbackEvent)
 def handle_postback(event):
