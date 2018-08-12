0e61f360416f7514a63bbfa4d4bb39311e73cb3a
Max Zheng <34936931+54bp6cl6@users.noreply.github.com>
2018-08-12 16:51:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -83,8 +83,8 @@ def Signup(index,text):
             'sheetUrl':"https://docs.google.com/spreadsheets/d/118ZANXoqpYW9BA5MTr58QsWKt1ZkxIphVRS6tZ3dzqo/edit#gid=0",
             'sheetTag':"成員列表",
             'type':"change",
-            'x':index+1,
-            'y':1,
+            'x':str(index+1),
+            'y':'1',
             'data':text
         }
         requests.get(url, params=payload)
