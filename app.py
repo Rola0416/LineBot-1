09d4ac5799f9e371f6a42a8d717ad6acb8cea89c
Max Zheng <34936931+54bp6cl6@users.noreply.github.com>
2018-08-14 12:46:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from linebot.exceptions import (
 )
 from linebot.models import *
 import requests
+import random
 
 app = Flask(__name__)
 
@@ -33,16 +34,15 @@ def callback():
 
 #user類別
 class user:
-    def __init__(self,ID,Name,Status):
-        self.Name = Name
+    def __init__(self,ID,Q):
         self.ID = ID
-        self.Status = Status
+        self.num = Q
 
 #讀取成員名單
 def GetUserList():
     url = "https://script.google.com/macros/s/AKfycbwVs2Si91yKz6m3utpaPtsttbh_lUQ8LOQM3Zud2hPFxXCgW3u1/exec"
     payload = {
-        'sheetUrl':"https://docs.google.com/spreadsheets/d/118ZANXoqpYW9BA5MTr58QsWKt1ZkxIphVRS6tZ3dzqo/edit#gid=0",
+        'sheetUrl':"https://docs.google.com/spreadsheets/d/1mlxmS0MYfBHSqiRLjaKyqNBvYZZHIGLwsbxr6FENdn4/edit#gid=0",
         'sheetTag':"成員列表",
         'row': 1,
         'col': 1,
@@ -55,8 +55,8 @@ def GetUserList():
     i = 0
     while i < len(temp):
         if temp[i] != "":
-            userlist.append(user(temp[i],temp[i+1],temp[i+2]))
-            i+=3
+            userlist.append(user(temp[i],temp[i+1]))
+            i+=2
         else:
             break
     return userlist
@@ -69,14 +69,53 @@ def Login(user_id,userlist):
     return -1
 
 #註冊
-def Signup(user_id,name):
+def Signup(user_id):
     url = "https://script.google.com/macros/s/AKfycbxn7Slc2_sKHTc6uEy3zmm3Bh_4duiGCXLavUM3RB0a3yzjAxc/exec"
     payload = {
-        'sheetUrl':"https://docs.google.com/spreadsheets/d/118ZANXoqpYW9BA5MTr58QsWKt1ZkxIphVRS6tZ3dzqo/edit#gid=0",
+        'sheetUrl':"https://docs.google.com/spreadsheets/d/1mlxmS0MYfBHSqiRLjaKyqNBvYZZHIGLwsbxr6FENdn4/edit#gid=0",
         'sheetTag':"成員列表",
-        'data':user_id+','+name+',學生'
+        'data':user_id+',-1'
     }
     requests.get(url, params=payload)
+    
+def Write(x,data):
+    url = "https://script.google.com/macros/s/AKfycbyBbQ1lsq4GSoKE0yiU5d6x0z2EseeBNZVTewWlSZhQ6EVrizo/exec"
+    payload = {
+        'sheetUrl':"https://docs.google.com/spreadsheets/d/1mlxmS0MYfBHSqiRLjaKyqNBvYZZHIGLwsbxr6FENdn4/edit#gid=0",
+        'sheetTag':"成員列表",
+        'data':data,
+        'x':str(x),
+        'y':'2'
+    }
+    requests.get(url, params=payload)
+    
+#對答案
+def Ans(Q, ans):
+    if Q == 0:
+        if ans.find('芝麻街') >= 0:
+            return "答對了"
+        else:
+            return "錯，答案是「芝麻街」，因為芝麻街美語(沒雨)"
+    elif Q == 1:
+        if ans.find('冰淇淋') >= 0:
+            return "答對了"
+        else:
+            return "錯，答案是「冰淇淋(麒麟)」"
+    elif Q == 2:
+        if ans.find('平行線') >= 0:
+            return "答對了"
+        else:
+            return "錯，答案是「平行線」，因為沒有相交(香蕉)"
+    elif Q == 3:
+        if ans.find('虱目魚') >= 0 or ans.find('濕木魚') >= 0:
+            return "答對了"
+        else:
+            return "錯，答案是「虱目魚」(濕木魚)"
+    elif Q == 4:
+        if ans.find('布丁狗') >= 0 or ans.find('不叮狗') >= 0:
+            return "答對了"
+        else:
+            return "錯，答案是「布丁狗」(不叮狗)"
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
@@ -84,27 +123,154 @@ def handle_message(event):
     try:
         userlist = GetUserList()
         clientindex = Login(event.source.user_id,userlist)
-        if clientindex > -1:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=userlist[clientindex].Name))
-            #開始使用功能
-        else:
-            message = TemplateSendMessage(
-                alt_text='確認姓名(手機限定)',
-                template=ConfirmTemplate(
-                    text='初次使用需要登記姓名\n您叫做'+event.message.text+'嗎?',
-                    actions=[
-                        PostbackTemplateAction(
-                            label='對',
-                            data='0`t`'+event.message.text
-                        ),
-                        PostbackTemplateAction(
-                            label='不對',
-                            data='0`f'
+        if clientindex == -1:
+            Signup(event.source.user_id)
+            clientindex = Login(event.source.user_id,userlist)
+        Q = int(userlist[clientindex].Q)
+        #開始使用功能
+        if Q < 0:
+            ran = random.randint(0, 9)
+            if ran == 0:
+                line_bot_api.reply_message(event.reply_token,TextSendMessage(text="哪一條街永遠不下雨?"))
+            elif ran == 1:
+                line_bot_api.reply_message(event.reply_token,TextSendMessage(text="麒麟飛到北極會變成什麼?"))
+            elif ran == 2:
+                line_bot_api.reply_message(event.reply_token,TextSendMessage(text="猴子最討厭哪一種線?"))
+            elif ran == 3:
+                line_bot_api.reply_message(event.reply_token,TextSendMessage(text="木魚掉進海裡會變成什麼?"))
+            elif ran == 4:
+                line_bot_api.reply_message(event.reply_token,TextSendMessage(text="有一隻蚊子牠只叮[鼠牛虎兔龍蛇馬羊猴雞豬]，請問這隻蚊子叫什麼名字?"))
+            elif ran == 5:
+                line_bot_api.reply_message(
+                    event.reply_token,TemplateSendMessage(
+                        alt_text='猜題面板(手機限定)',
+                        template=ButtonsTemplate(
+                            thumbnail_image_url='https://truth.bahamut.com.tw/s01/201709/e9c87c887640edf475d96f171eb8834f.PNG',
+                            title='題目',
+                            text='狼、老虎和獅子誰玩遊戲一定會被淘汰?',
+                            actions=[
+                                PostbackTemplateAction(
+                                    label='狼',
+                                    data='1`t'
+                                ),
+                                PostbackTemplateAction(
+                                    label='老虎',
+                                    data='1`f'
+                                ),
+                                PostbackTemplateAction(
+                                    label='獅子',
+                                    data='1`f'
+                                )
+                            ]
                         )
-                    ]
+                    )
                 )
-            )
-            line_bot_api.reply_message(event.reply_token, message)
+            elif ran == 6:
+                line_bot_api.reply_message(event.reply_token,
+                    TemplateSendMessage(
+                        alt_text='猜題面板(手機限定)',
+                        template=ButtonsTemplate(
+                            thumbnail_image_url='https://truth.bahamut.com.tw/s01/201709/e9c87c887640edf475d96f171eb8834f.PNG',
+                            title='題目',
+                            text='孔子有三位徒弟子貢、子路、和子游,請問哪一位不是人?',
+                            actions=[
+                                PostbackTemplateAction(
+                                    label='子貢',
+                                    data='1`f'
+                                ),
+                                PostbackTemplateAction(
+                                    label='子路',
+                                    data='1`t'
+                                ),
+                                PostbackTemplateAction(
+                                    label='子游',
+                                    data='1`f'
+                                )
+                            ]
+                        )
+                    )
+                )
+            elif ran == 7:
+                line_bot_api.reply_message(event.reply_token,
+                    TemplateSendMessage(
+                        alt_text='猜題面板(手機限定)',
+                        template=ButtonsTemplate(
+                            thumbnail_image_url='https://truth.bahamut.com.tw/s01/201709/e9c87c887640edf475d96f171eb8834f.PNG',
+                            title='題目',
+                            text='獅子、狼、熊，哪一隻的牙齒最好?',
+                            actions=[
+                                PostbackTemplateAction(
+                                    label='獅子',
+                                    data='1`f'
+                                ),
+                                PostbackTemplateAction(
+                                    label='狼',
+                                    data='1`t'
+                                ),
+                                PostbackTemplateAction(
+                                    label='熊',
+                                    data='1`f'
+                                )
+                            ]
+                        )
+                    )
+                )
+            elif ran == 8:
+                line_bot_api.reply_message(event.reply_token,
+                    TemplateSendMessage(
+                        alt_text='猜題面板(手機限定)',
+                        template=ButtonsTemplate(
+                            thumbnail_image_url='https://truth.bahamut.com.tw/s01/201709/e9c87c887640edf475d96f171eb8834f.PNG',
+                            title='題目',
+                            text='喵喵、吱吱 、旺旺誰會最先被叫起來背書?',
+                            actions=[
+                                PostbackTemplateAction(
+                                    label='喵喵',
+                                    data='1`f'
+                                ),
+                                PostbackTemplateAction(
+                                    label='吱吱',
+                                    data='1`f'
+                                ),
+                                PostbackTemplateAction(
+                                    label='旺旺',
+                                    data='1`t'
+                                )
+                            ]
+                        )
+                    )
+                )
+            elif ran == 9:
+                line_bot_api.reply_message(event.reply_token,
+                    TemplateSendMessage(
+                        alt_text='猜題面板(手機限定)',
+                        template=ButtonsTemplate(
+                            thumbnail_image_url='https://truth.bahamut.com.tw/s01/201709/e9c87c887640edf475d96f171eb8834f.PNG',
+                            title='題目',
+                            text='蝴蝶, 蜘蛛, 蜈蚣, 哪一個沒有領到酬勞?',
+                            actions=[
+                                PostbackTemplateAction(
+                                    label='蝴蝶',
+                                    data='1`f'
+                                ),
+                                PostbackTemplateAction(
+                                    label='蜘蛛',
+                                    data='1`f'
+                                ),
+                                PostbackTemplateAction(
+                                    label='蜈蚣',
+                                    data='1`t'
+                                )
+                            ]
+                        )
+                    )
+                )
+            Write(clientindex,ran)
+    elif Q < 5:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=Ans(Q ,event.message.text)))
+        Write(clientindex,-1)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="要回答請點擊題目下方的按鈕喔"))
     except:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text="發生錯誤01"))
     
@@ -120,6 +286,25 @@ def handle_postback(event):
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text="註冊成功，歡迎來到LineBot世界"))
         elif data[1] == 'f':
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text="請再次輸入您的姓名"))
+    elif data[0] == '1':
+        userlist = GetUserList()
+        clientindex = Login(event.source.user_id,userlist)
+        Q = int(userlist[clientindex].Q)
+        if data[1] == 't' and Q > 4:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="答對了"))
+        else:
+            if Q == 5:
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text="錯，答案是「狼」，因為「桃太郎」(淘汰狼)"))
+            elif Q == 6:
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text="錯，答案是「子路」，因為「指鹿(子路)為馬」"))
+            elif Q == 7:
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text="錯，答案是「狼」，因為「狼牙棒」"))
+            elif Q == 8:
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text="錯，答案是「旺旺」，因為「旺旺仙貝(先背)」"))
+            elif Q == 9:
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text="錯，答案是「蜈蚣」，因為「無功(蜈蚣)不受祿」"))
+        if Q > 4:
+            Write(clientindex,2)
     
 import os
 if __name__ == "__main__":
