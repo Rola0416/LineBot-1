2a7c012fe201beb97c9ae5d7585441ca0601c50d
Max Zheng <34936931+54bp6cl6@users.noreply.github.com>
2018-11-29 19:50:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,8 +33,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text = Reply(event.message.text))
-    line_bot_api.reply_message(event.reply_token, message)
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text = Reply(event.message.text)))
 
 def Reply(text):
     if str(text) == "hi" :
